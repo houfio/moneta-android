69db0c537e96e7f7788f034596efba1e4fcbf9d4
houfio <26674115+houfio@users.noreply.github.com>
2021-03-24 13:01:34 +0100

    Initial commit

---
+++ app/src/main/java/io/houf/moneta/ui/Color.kt
@@ -0,0 +1,6 @@
+package io.houf.moneta.ui
+
+import androidx.compose.ui.graphics.Color
+
+val Yellow600 = Color(0xffffb805)
+val Blue500 = Color(0xff054cff)
---
+++ app/src/main/java/io/houf/moneta/ui/Theme.kt
@@ -0,0 +1,33 @@
+package io.houf.moneta.ui
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = Yellow600,
+    secondary = Blue500
+)
+
+private val LightColorPalette = lightColors(
+    primary = Yellow600,
+    secondary = Blue500
+)
+
+@Composable
+fun MonetaTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
