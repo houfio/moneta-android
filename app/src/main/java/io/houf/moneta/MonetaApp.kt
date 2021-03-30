d3f46fcec26a4f26b959fce377f8fafd5708b8cc
houfio <26674115+houfio@users.noreply.github.com>
2021-03-30 15:15:45 +0200

    Remove navigationbar background color

--- app/src/main/java/io/houf/moneta/MonetaApp.kt
+++ app/src/main/java/io/houf/moneta/MonetaApp.kt
@@ -1,10 +1,12 @@
 package io.houf.moneta
 
 import android.content.Context
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.*
 import io.houf.moneta.Screen.*
 import io.houf.moneta.view.PortfolioView
@@ -18,25 +20,32 @@ fun MonetaApp(context: Context) {
 
     Scaffold(
         bottomBar = {
-            BottomNavigation(backgroundColor = MaterialTheme.colors.primary) {
-                val navigationState by controller.currentBackStackEntryAsState()
-                val currentRoute = navigationState?.arguments?.getString(KEY_ROUTE)
+            Column {
+                Divider(color = LocalContentColor.current.copy(alpha = 0.25f))
+                BottomNavigation(
+                    backgroundColor = MaterialTheme.colors.background,
+                    elevation = 0.dp
+                ) {
+                    val navigationState by controller.currentBackStackEntryAsState()
+                    val currentRoute = navigationState?.arguments?.getString(KEY_ROUTE)
 
-                screens.forEach { screen ->
-                    val label = stringResource(screen.resourceId)
+                    screens.forEach { screen ->
+                        val label = stringResource(screen.resourceId)
 
-                    BottomNavigationItem(
-                        selected = currentRoute == screen.route,
-                        icon = { Icon(screen.icon, label) },
-                        label = { Text(label) },
-                        alwaysShowLabel = false,
-                        onClick = {
-                            controller.navigate(screen.route) {
-                                popUpTo = controller.graph.startDestination
-                                launchSingleTop = true
+                        BottomNavigationItem(
+                            selected = currentRoute == screen.route,
+                            icon = { Icon(screen.icon, label) },
+                            label = { Text(label) },
+                            selectedContentColor = MaterialTheme.colors.primary,
+                            unselectedContentColor = LocalContentColor.current,
+                            onClick = {
+                                controller.navigate(screen.route) {
+                                    popUpTo = controller.graph.startDestination
+                                    launchSingleTop = true
+                                }
                             }
-                        }
-                    )
+                        )
+                    }
                 }
             }
         }
