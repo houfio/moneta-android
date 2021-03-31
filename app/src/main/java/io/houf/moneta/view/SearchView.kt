f34186d42f22d598cb406575b5ee2f7b0a8784f0
houfio <26674115+houfio@users.noreply.github.com>
2021-03-31 23:21:33 +0200

    Add clickable to search row

--- app/src/main/java/io/houf/moneta/view/SearchView.kt
+++ app/src/main/java/io/houf/moneta/view/SearchView.kt
@@ -1,5 +1,6 @@
 package io.houf.moneta.view
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,11 +17,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltNavGraphViewModel
 import io.houf.moneta.R
 import io.houf.moneta.Screen
+import io.houf.moneta.activity.ListingActivity
+import io.houf.moneta.activity.ListingData
+import io.houf.moneta.util.openActivity
 import io.houf.moneta.view.component.Pill
 import io.houf.moneta.view.component.TopBar
 import io.houf.moneta.viewmodel.SearchViewModel
@@ -36,11 +41,19 @@ fun SearchView(viewModel: SearchViewModel = hiltNavGraphViewModel()) {
     }
     LazyColumn {
         items(listings) { listing ->
+            val context = LocalContext.current
             val change by viewModel.change(listing)
 
             Row(
                 verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier
+                    .clickable {
+                        openActivity(
+                            context,
+                            ListingActivity::class.java,
+                            ListingData(listing)
+                        )
+                    }
                     .padding(horizontal = 16.dp, vertical = 12.dp)
                     .fillMaxWidth()
             ) {
